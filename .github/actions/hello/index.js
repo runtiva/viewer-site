77e911c48a485b0aedb005b84887a176e7af464e
runtiva25@gmail.com <runtiva25@gmail.com>
2021-01-18 10:30:47 -0600

    ci(updated hello action): updated hello action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,9 +2,14 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  throw(new Error("some error message"));
+  // throw(new Error("some error message"));
+
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message"); // does not cause action to fail
 
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
